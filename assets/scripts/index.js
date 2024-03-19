862459e998888fb7b63dd6cd275eed7da2eca000
enowjohn <148995120+enowjohn@users.noreply.github.com>
2024-03-19 18:15:40 +0100

    removing all the semile colomns in js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,33 +1,33 @@
 function calculateHandshake () {
-  const inputNumber = parseInt(document.getElementById('numberinput').value);
+  const inputNumber = parseInt(document.getElementById('numberinput').value)
   const binaryString = inputNumber.toString(2); // Convert to binary string
 
-  const handshake = [];
+  const handshake = []
 
   // Check each bit of the binary string
   for (let i = binaryString.length - 1; i >= 0; i--) {
     const bit = binaryString[i];
-    const positionFromRight = binaryString.length - 1 - i;
+    const positionFromRight = binaryString.length - 1 - i
 
     if (bit === '1') {
       if (positionFromRight === 0) {
-        handshake.push('wink');
+        handshake.push('wink')
       } else if (positionFromRight === 1) {
-        handshake.push('double blink');
+        handshake.push('double blink')
       } else if (positionFromRight === 2) {
-        handshake.push('close your eyes');
+        handshake.push('close your eyes')
       } else if (positionFromRight === 3) {
         handshake.push('jump');
       } else if (positionFromRight === 4) {
-        handshake.reverse(); // Reverse the handshake if the last bit is set
+        handshake.reverse() // Reverse the handshake if the last bit is set
       }
     }
   }
 
-  displayHandshake(handshake);
+  displayHandshake(handshake)
 }
 
 function displayHandshake(handshake) {
-  const handshakeResult = document.getElementById('handshakeresult');
-  handshakeResult.textContent = handshake.join(', ');
+  const handshakeResult = document.getElementById('handshakeresult')
+  handshakeResult.textContent = handshake.join(', ')
 }
