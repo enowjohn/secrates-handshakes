c74875fc0ffd1ef59d14ae8c6958e501797a3dee
enowjohn <148995120+enowjohn@users.noreply.github.com>
2024-03-19 18:27:42 +0100

    removing all the semile colomns in js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,6 @@
 function calculateHandshake () {
   const inputNumber = parseInt(document.getElementById('numberinput').value)
-  const binaryString = inputNumber.toString(2);// Convert to binary string
+  const binaryString = inputNumber.toString(2)// Convert to binary string
 
   const handshake = []
 
@@ -31,3 +31,6 @@ function displayHandshake (handshake) {
   const handshakeResult = document.getElementById('handshakeresult')
   handshakeResult.textContent = handshake.join(', ')
 }
+
+calculateHandshake ()
+
